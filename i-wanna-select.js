420a246a79f8d369eabf2a146b85fb5c9b5e2782
The-HooK <joel.2.rovera@gmail.com>
2016-05-25 11:51:31 +0200

    Update i-wanna-select.js

--- i-wanna-select.js
+++ i-wanna-select.js
@@ -1,12 +1,10 @@
-function iWannaSelect() {
-  var elmt = document.getElementsByClassName('SansSelectionTexte');
-  for (i = 0; i < elmt.length; i++) {
-    elmt[i].style.mozUserSelect = '-moz-text';
-    elmt[i].style.webkitUserSelect = 'text';
-    elmt[i].style.webkitTouchCtextout = 'text';
-    elmt[i].style.khtmlUserSelect = 'text';
-    elmt[i].style.msUserSelect = 'text';
-    elmt[i].style.userSelect = 'text';
-    console.log('Now you can select what ever you want ! ;)');
-  }
+var elmt = document.getElementsByClassName('SansSelectionTexte');
+for (i = 0; i < elmt.length; i++) {
+  elmt[i].style.mozUserSelect = '-moz-text';
+  elmt[i].style.webkitUserSelect = 'text';
+  elmt[i].style.webkitTouchCtextout = 'text';
+  elmt[i].style.khtmlUserSelect = 'text';
+  elmt[i].style.msUserSelect = 'text';
+  elmt[i].style.userSelect = 'text';
+  console.log('Now you can select what ever you want ! ;)');
 }
